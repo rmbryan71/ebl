a40336a2f9d9fe5c8d8f433fa7bd0dfdc00da92f
robertbryan <rob@robertbryan.net>
2026-01-17 13:06:22 -0500

    fixes demo league create bug

--- scripts/make-demo-league.py
+++ scripts/make-demo-league.py
@@ -1,7 +1,12 @@
 import random
 import re
+import sys
 
 from werkzeug.security import generate_password_hash
+
+ROOT_DIR = Path(__file__).resolve().parents[1]
+if str(ROOT_DIR) not in sys.path:
+    sys.path.insert(0, str(ROOT_DIR))
 
 from db import get_connection, ensure_identities
 
